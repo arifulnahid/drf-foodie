4824119ee39d08d2b2bfb90eca2f758e5c492610
arifulnahid <ainahidfsti@gmail.com>
2024-12-30 21:45:31 +0600

    clear

--- account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.UserRegistrationView.as_view(), name='register'),
-    path('login/', views.UserLoginView.as_view(), name='login'),
-    path('logout/', views.UserLogoutView.as_view(), name='logout'),
-    path('active/<uid64>/<token>/', views.activate, name = 'activate'),
-]
